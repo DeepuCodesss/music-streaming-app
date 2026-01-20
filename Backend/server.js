252d1688a8628cfde9286a0a581d1048bfad8204
deepucodesss <deepakmangal94164@gmail.com>
2026-01-21 00:33:05 +0530

    Auto-load songs from folder

--- Backend/server.js
+++ Backend/server.js
@@ -1,28 +1,42 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
-app.use(cors());
-
-
-app.use(express.static(path.join(__dirname, "public")));
-
-
 const PORT = 3000;
 
+// enable CORS
+app.use(cors());
+
+// serve frontend files
+app.use(express.static(path.join(__dirname, "public")));
+
+// serve song files
 app.use("/songs", express.static(path.join(__dirname, "songs")));
 
-const songs = [
-  { id: 1, title: "Song One", file: "song1.mp3" },
-  { id: 2, title: "Song Two", file: "song2.mp3" },
-  { id: 3, title: "Song Three", file: "song3.mp3" }
-];
 
 app.get("/songs-list", (req, res) => {
-  res.json(songs);
+  const songsDir = path.join(__dirname, "songs");
+
+  fs.readdir(songsDir, (err, files) => {
+    if (err) {
+      return res.status(500).json({ error: "Unable to read songs folder" });
+    }
+
+    const songs = files
+      .filter(file => file.endsWith(".mp3"))
+      .map((file, index) => ({
+        id: index + 1,
+        title: file.replace(".mp3", ""),
+        file: file
+      }));
+
+    res.json(songs);
+  });
 });
 
+// start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
