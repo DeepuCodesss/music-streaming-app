12488f5ca0c05429775029959d046f764bd43396
deepucodesss <deepakmangal94164@gmail.com>
2026-01-20 22:28:32 +0530

    added search engine

--- Backend/public/script.js
+++ Backend/public/script.js
@@ -1,7 +1,12 @@
 const audio = document.getElementById("audio");
 const songsList = document.getElementById("songs");
+const searchInput = document.getElementById("search");
+
+let allSongs = [];
+
 
 songsList.innerHTML = "<li>Loading songs...</li>";
+
 
 fetch("/songs-list")
   .then(res => {
@@ -9,22 +14,41 @@ fetch("/songs-list")
     return res.json();
   })
   .then(songs => {
-    songsList.innerHTML = "";
-
-    songs.forEach(song => {
-      const li = document.createElement("li");
-      li.textContent = song.title;
-      li.style.cursor = "pointer";
-
-      li.onclick = () => {
-        audio.src = `http://localhost:3000/songs/${song.file}`;
-        audio.play();
-      };
-
-      songsList.appendChild(li);
-    });
+    allSongs = songs;
+    renderSongs(allSongs);
   })
   .catch(err => {
-    songsList.innerHTML = "<li style='color:red'>Backend not running</li>";
+    songsList.innerHTML =
+      "<li style='color:red'>Backend not running</li>";
     console.error(err);
   });
+
+
+function renderSongs(songs) {
+  songsList.innerHTML = "";
+
+  songs.forEach(song => {
+    const li = document.createElement("li");
+    li.textContent = song.title;
+    li.style.cursor = "pointer";
+    li.style.margin = "8px 0";
+
+    li.onclick = () => {
+      audio.src = `/songs/${song.file}`;
+      audio.play();
+    };
+
+    songsList.appendChild(li);
+  });
+}
+
+
+searchInput.addEventListener("input", () => {
+  const query = searchInput.value.toLowerCase();
+
+  const filteredSongs = allSongs.filter(song =>
+    song.title.toLowerCase().includes(query)
+  );
+
+  renderSongs(filteredSongs);
+});
