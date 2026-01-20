cfa854e45af2309e22eb463a3ca4217b11bf043c
deepucodesss <deepakmangal94164@gmail.com>
2026-01-20 13:17:38 +0530

    Fixed Cors and connected frontend with backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
+
 const PORT = 3000;
 
 app.use("/songs", express.static(path.join(__dirname, "songs")));
--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,30 @@
 const audio = document.getElementById("audio");
+const songsList = document.getElementById("songs");
 
-function playMusic() {
-  audio.play();
-}
+songsList.innerHTML = "<li>Loading songs...</li>";
 
-function pauseMusic() {
-  audio.pause();
-}
+fetch("http://localhost:3000/songs-list")
+  .then(res => {
+    if (!res.ok) throw new Error("Backend not reachable");
+    return res.json();
+  })
+  .then(songs => {
+    songsList.innerHTML = "";
+
+    songs.forEach(song => {
+      const li = document.createElement("li");
+      li.textContent = song.title;
+      li.style.cursor = "pointer";
+
+      li.onclick = () => {
+        audio.src = `http://localhost:3000/songs/${song.file}`;
+        audio.play();
+      };
+
+      songsList.appendChild(li);
+    });
+  })
+  .catch(err => {
+    songsList.innerHTML = "<li style='color:red'>Backend not running</li>";
+    console.error(err);
+  });
